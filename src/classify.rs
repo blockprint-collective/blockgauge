f7eb8200efd2479289118705e23cf0e8bc84f353
Michael Sproul <michael@sigmaprime.io>
2023-06-14 16:41:59 +1000

    New accuracy tracking (#1)
    * More detailed accuracy tracking
    
    * Tweak config
    
    * Rename some fields

--- src/classify.rs
+++ src/classify.rs
@@ -1,11 +1,21 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::collections::HashMap;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ClassifyRequest {
     pub names: Vec<String>,
     pub labels: Vec<String>,
-    pub blocks: Vec<Value>,
+    pub blocks: Vec<Block>,
+}
+
+/// Skeleton type for pulling out the slot of each block.
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Block {
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub slot: u64,
+    #[serde(flatten)]
+    pub rest: HashMap<String, Value>,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
